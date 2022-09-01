1fa537d1260b7854a82380360f2a527c4ca06271
Douglas A B Novato <douglasabnovato@gmail.com>
2022-09-01 12:18:55 -0300

    projeto css in js com styled components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom' 
+import {createRoot} from 'react-dom/client';
+import {StrictMode} from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from './App'
+
+import GlobalStyle from './assets/styles/global-style'
+import { ThemeProvider } from 'styled-components'
+import { media } from './assets/styles/media'
+
+const theme = {
+  primary: '#202020',
+  secondary: '#393939'
+}
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement); 
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <GlobalStyle />
+    <ThemeProvider
+      theme={{
+        ...theme,
+        ...media
+      }}
+    >
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ 
